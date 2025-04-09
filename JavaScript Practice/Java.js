d55876e5e5b0a2eb9a28bd38fee2da55d28ac131
ShankarsinghRajput <sshankarsingh60@gmail.com>
2025-04-09 13:00:57 +0530

    update

--- JavaScript Practice/Java.js
+++ JavaScript Practice/Java.js
@@ -2,47 +2,103 @@
 // marks = Number(marks); // convert input to number
 
 
-// if (marks > 90) {
-//     console.log("Wonderful! You are a topper! 🎉");
+// if (marks < 100) {
+//     console.log("Congratulations you have done a great job! 🎉");
 // } else if (marks > 75) {
-//     console.log("Amazing! Great job! 🌟");
+//     console.log("Amazing! youre doing better! 🌟");
+     
 // } else {
-//     console.log("Keep improving! You can do it! 💪");
+//     console.log("Keep improving you really need to work ! You can do it! 💪");
 // }
 
 
-for (let i = 3; i < 9; i++) {
-    console.log(i);
-  }
+// for (let i = 3; i < 9; i++) {
+//     console.log(i);
+//   }
   
-  function say(name) {
-    return "SUPRABHAT " + name;
-  }
-  console.log(say("RONDO"));
+//   function say(name) {
+//     return "SUPRABHAT " + name;
+//   }
+//   console.log(say("RONDO"));
   
-  let fruits = ["apple", "banana", "cherry"];
-fruits.push("mango","orange");
-console.log(fruits);
+//   let fruits = ["apple", "banana", "cherry"];
+// fruits.push("mango","orange");
+// console.log(fruits);
 
-function saveData() {
-    let person = {
-        name: document.getElementById("nameInput").value,
-        age: document.getElementById("ageInput").value,
-        city: document.getElementById("cityInput").value,
-        email: document.getElementById("emailInput").value
-    };
+// function saveData() {
+//     let person = {
+//         name: document.getElementById("nameInput").value,
+//         age: document.getElementById("ageInput").value,
+//         city: document.getElementById("cityInput").value,
+//         email: document.getElementById("emailInput").value
+//     };
 
-    console.log(person);
-}
+//     console.log(person);
+// }
 
 
-// let person = {
-//     name: "Ranu Singh",
-//     age: 21,
-//     Father: "Aaditya",
-//     child: "Kaalu- aka Emmanuel",
-//     city: "Mumbai",
-//     email:"RANUSINGH@gmail.com"
-//   };
-//   console.log(person.child, person.Father);
+// // let person = {
+// //     name: "Ranu Singh",
+// //     age: 21,
+// //     Father: "Aaditya",
+// //     child: "Kaalu- aka Emmanuel",
+// //     city: "Mumbai",
+// //     email:"RANUSINGH@gmail.com"
+// //   };
+// //   console.log(person.child, person.Father);
+//  let a=7;
+//  let b =7;
+//  let c = a+b;
+//  console.log(c); 
+
+//  let e=200;
+//  let f =600;
+//  let d = e-f;
+//  console.log(d); 
+
+ 
+//  let g=200;
+//  let h =6000;
+//  let i = g*h;
+//  console.log(i);
+
+//  let l=200;
+//  let k =6000;
+//  let z = l/k;
+//  console.log(z);
+
+//  let v=200;
+//  let m =600;
+//  let n = v%m;
+//  console.log(n);
+//  console.log(a++);
+//  console.log(++a);
+// console.log(f--);
+// console.log(--f);
+
+let length = 2;
+let width = 7;
+
+let area = length * width;
+console.log(area);  
+// = 14
+
+let a= 20;
+let b= 25;
+let c = 70;
+let d = 50;
+
+let sum = a + b + c + d/4 ;
+console.log(sum);
+// =127.5
+
+let e = 3;
+let f = 2;
+console.log(e+"  "+f)
+let g = e ;
+ e = f;
+ f = g ;
+console.log(e+"  "+f)
   
+
+
